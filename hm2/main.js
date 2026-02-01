e8c8ea45e940b51c95ba030a9f383a37a3ab55aa
Viktoriia <vikatimoshenko126@gmail.com>
2026-02-01 23:36:04 +0100

    chnages

--- hm2/main.js
+++ hm2/main.js
@@ -3,9 +3,14 @@ let result2 = (10 / 2) ** 2;
 let result3 = 2 * (4 + 3) - (8 / 2);
 let result4 = (15 % 4) + 5;
 let result5 = 3 - (2 ** 4);
+let myResult1 = (result1 + result2 - result4) / 2;
+let myResult2 = 4 * (2 ** 2);
 
 console.log(result1);
 console.log(result2);
 console.log(result3);
 console.log(result4);
 console.log(result5);
+
+console.log(myResult1);
+console.log(myResult2);
